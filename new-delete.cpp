fd85dba7c003850316a666b85e166ac7b647135e
David Mrnustik <david.mrnustik@hotmail.com>
2022-04-11 16:26:49 +0200

    new delete allocation

---
+++ new-delete.cpp
@@ -0,0 +1,44 @@
+//
+// Created by David Mrnustik on 11/4/22.
+//
+#include <cstdio>
+#include <new>
+using namespace std;
+
+class c1 {
+    int _a = 0;
+    int _b = 0;
+    int _c = 0;
+public:
+    c1(int i = 0);
+    ~c1();
+    int a() { return _a; }
+    int b() { return _b; }
+    int c() { return _c; }
+};
+
+c1::c1(int i) : _a(i), _b(i + 1), _c(i + 2) {
+    puts("c1 constructor");
+}
+
+c1::~c1() {
+    puts("c1 destructor");
+}
+
+int main() {
+    /* an alternative version when it's not possible to catch an exception
+     * c1 *o1 = new(nothrow) c1
+     * if (o1 == nullptr) { puts("new c1 failed"); return 1; }
+     */
+    try {
+        c1 *o1 = new c1;
+        c1 *o2 = new c1[5]; // in case of array
+        printf("o1: %d, %d, %d\n", o1->a(), o1->b(), o1->c());
+        delete o1;
+        delete[] o2;
+    } catch(bad_alloc &ba) {
+        printf("new c1 failed: %s\n", ba.what());
+        return 1;
+    }
+    return 0;
+}
